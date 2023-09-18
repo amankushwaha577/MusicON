a07d746ba2187aeb95791adee981e10ef0349560
Aman Kushwaha <aman.kushwaha_cs18@gla.ac.in>
2023-09-18 23:40:30 +0530

    Brand

--- src/components/fragment/Brand.jsx
+++ src/components/fragment/Brand.jsx
@@ -1,18 +1,14 @@
 import React from "react";
 import {Link} from "react-router-dom";
 import "../assets/scss/Brand.scss";
-import Logo from "../assets/img/headphonesLogo.svg"
+import Logo from "../assets/img/logo.png"
 
 class Brand extends React.Component {
     render() {
         return (
             <div  className={"brand"}>
                 <Link to={"/home"}>
-                    <h1>
-                        V
-                        <img src={Logo} width={"24px"} alt=""/>
-                        sic
-                    </h1>
+                        <img src={Logo} width={"190px"} alt=""/>
                 </Link>
             </div>
         );
